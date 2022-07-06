fc95e239a7d258c27699d7258becb9d249a2a154
Oleg Gavrilin <oleg@minterest.com>
2022-07-06 17:09:50 +0300

    add svg with url

--- rollup.config.js
+++ rollup.config.js
@@ -27,7 +27,7 @@ export default [
     ],
     plugins: [
       url({
-        include: ['**/*.ttf'],
+        include: ['**/*.ttf', '**/*.svg'],
         limit: Infinity,
       }),
       svgr(),
