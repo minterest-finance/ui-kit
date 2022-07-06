591687b3bbcb035a9a22bbc04b444ad0c07f33da
Oleg Gavrilin <oleg@minterest.com>
2022-07-06 16:11:06 +0300

    workaround for types

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,46 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
-import url from 'rollup-plugin-url'
-
-import { terser } from "rollup-plugin-terser";
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+import url from 'rollup-plugin-url';
 
-
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            url({
-                // by default, rollup-plugin-url will not handle font files
-                include: ['**/*.ttf', '**/*.svg'],
-                // setting infinite limit will ensure that the files
-                // are always bundled with the code, not copied to /dist
-                limit: Infinity,
-            }),
-            peerDepsExternal(),
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: "./module.tsconfig.json" }),
-            postcss(),
-            terser(),
-        ],
-    },
-    {
-        input: "dist/esm/types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
-
-        // NEW
-        external: [/\.scss$/, /\.css$/,  /\.svg$/,  /\.ttf/, ],
-    },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      url({
+        include: ['**/*.ttf', '**/*.svg'],
+        limit: Infinity,
+      }),
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './module.tsconfig.json' }),
+      postcss(),
+      terser(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.scss$/, /\.css$/, /\.svg$/, /\.ttf/],
+  },
 ];
