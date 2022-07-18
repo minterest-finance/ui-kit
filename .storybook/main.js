1e08f59f7b1c193550a9518c71d578c26a92aa51
Oleg Gavrilin <oleg@minterest.com>
2022-07-18 16:15:08 +0300

    change public path

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,5 +9,6 @@ module.exports = {
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  "publicPath": process.env.PUBLIC_URL
 }
