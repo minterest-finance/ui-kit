b2301cb96c6377465f6e3bae4f2ad7ae84387405
p1geon <tasker10024@gmail.com>
2022-07-18 16:12:14 +0300

    [MUI-416] Created numeric info component

---
+++ src/components/Table/NumericInfo/NumericInfo.tsx
@@ -0,0 +1,104 @@
+import React, { FC, SVGProps } from 'react';
+
+import { styled } from '@mui/material/styles';
+import ContentLoader from 'react-content-loader';
+
+import { Typography } from 'components';
+
+type Props = {
+    usdValue: string,
+    assetValue: string,
+    isLoading: boolean,
+};
+
+const ElementWrapper = styled('div')({
+    width: 80,
+    height: 41,
+    display: 'flex',
+    justifyContent: 'flex-end',
+});
+
+const DataWrapper = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+});
+
+const NoDataWrapper = styled('div')({
+    display: 'flex',
+    alignItems: 'center',
+});
+
+const GreyText = styled(Typography)({
+    color: '#6D7692',
+    textAlign: 'right'
+});
+
+const BlueText = styled(Typography)({
+    color: '#061953',
+});
+
+const UsdValueWrapper = styled('div')({
+    textAlign: 'right',
+    // todo DO BETTER
+    fontSize: 0,
+    lineHeight: 0,
+});
+
+const Loader: React.FC = () => (
+    <ContentLoader height={21} width={64}>
+      <rect x={0} y={0} rx={4} ry={4} width={64} height={21} />
+    </ContentLoader>
+  );
+
+const NumericInfo: FC<Props> = ({
+    usdValue,
+    assetValue,
+    isLoading
+}: Props) => {
+
+    if (isLoading) return (
+        <ElementWrapper>
+            <NoDataWrapper>
+                <Loader />
+            </NoDataWrapper>
+        </ElementWrapper>
+    );
+
+    const bothValuesAreZero = (!usdValue || usdValue === '0') && (!assetValue || assetValue === '0');
+    return (
+        <ElementWrapper>
+            {!bothValuesAreZero && (
+                <DataWrapper>
+                <UsdValueWrapper>
+                    <GreyText
+                        variant={'table2'}
+                        text={'$'}
+                    />
+                    <BlueText
+                        variant={'table2'}
+                        text={usdValue}
+                    />
+                </UsdValueWrapper>
+                <>
+                    <GreyText
+                        variant={'subtitle2'}
+                        text={assetValue}
+                    />
+                </>
+            </DataWrapper>
+            )}
+
+            {bothValuesAreZero && (
+                <NoDataWrapper>
+                    <GreyText
+                        variant={'table1'}
+                        text={'—'}
+                    />
+                </NoDataWrapper>
+            )}
+        </ElementWrapper>
+    )
+};
+
+export default NumericInfo;
