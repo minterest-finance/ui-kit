02b1fccaded306b991b13ac5b586317cc6293289
Oleg Gavrilin <oleg@minterest.com>
2022-07-18 14:19:12 +0300

    fix patch

---
+++ .circleci/publish-github-deployment.sh
@@ -0,0 +1,43 @@
+# !/bin/bash
+
+set -e
+
+EXISTING_DEPLOYMENT_IDS=$(curl -s \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token $GITHUB_DEPLOYMENT_TOKEN" \
+    https://api.github.com/repos/minterest-finance/$CIRCLE_PROJECT_REPONAME/deployments?ref=$CIRCLE_BRANCH \
+  | jq .[].id)
+
+for ID in $EXISTING_DEPLOYMENT_IDS
+do
+  echo "Deleting existing Github deployment $ID"
+
+  curl -s -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token $GITHUB_DEPLOYMENT_TOKEN" \
+    https://api.github.com/repos/minterest-finance/$CIRCLE_PROJECT_REPONAME/deployments/$ID/statuses \
+    -d '{"state":"inactive"}' > /dev/null
+
+  curl -s -X DELETE \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token $GITHUB_DEPLOYMENT_TOKEN" \
+    https://api.github.com/repos/minterest-finance/$CIRCLE_PROJECT_REPONAME/deployments/$ID > /dev/null
+done
+
+DEPLOYMENT=$(curl -s -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_DEPLOYMENT_TOKEN" \
+  https://api.github.com/repos/minterest-finance/$CIRCLE_PROJECT_REPONAME/deployments \
+  -d '{"ref":"'"$CIRCLE_BRANCH"'","environment":"temp","transient_environment":true,"required_contexts":[]}')
+
+DEPLOYMENT_ID=$(echo ${DEPLOYMENT} | jq .id)
+
+echo "Publishing a new Github deployment $DEPLOYMENT_ID"
+
+curl -s -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_DEPLOYMENT_TOKEN" \
+  https://api.github.com/repos/minterest-finance/$CIRCLE_PROJECT_REPONAME/deployments/$DEPLOYMENT_ID/statuses \
+  -d '{"state":"success","environment_url":"'"$WEBSITE_URL"'","auto_inactive":true}' > /dev/null
+
+echo "Successfully deployed to $WEBSITE_URL"
