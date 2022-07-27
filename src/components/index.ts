5e1b82e04d14de8a6a0a1f68bc5ede4f18baba27
Oleg Gavrilin <oleg@minterest.com>
2022-07-27 16:59:54 +0300

    fix lint

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,7 @@
 export { default as Typography } from './Typography/Typography';
 export * from './Button/Button';
 export * from './StatsWing/StatsWing';
-<<<<<<< HEAD
-=======
 export * from './StatsWing/StatsWingCircle';
 export * from './StatsWing/StatsWingTextBlock';
->>>>>>> origin/main
 export * from './HeaderMessage/HeaderMessage';
 export * from './Table/index';
