620e5a9c4890f3c34c3a6b7009cf1105dedb0508
p1geon <tasker10024@gmail.com>
2022-07-28 16:34:21 +0300

    [MUI-420] Create Table Header component (#10)
    * [MUI-420] Created Header Category component
    
    * [MUI-420] Addressed some comments
    
    * [MUI-420] Linter
    
    Co-authored-by: Anton Dolmat <anton.dolmat@gmail.com>

--- src/components/Table/index.ts
+++ src/components/Table/index.ts
@@ -1,4 +1,5 @@
 export { default as AssetName } from './AssetName/AssetName';
+export { default as HeaderCategory } from './HeaderCategory/HeaderCategory';
 export { default as NumericInfo } from './NumericInfo/NumericInfo';
 export { default as MNTReward } from './MNTReward/MNTReward';
 export { default as PercentageInfo } from './PercentageInfo/PercentageInfo';
