22a6adf8389be2e73a419c0e29f05ffaaa00b817
Maksim <46103779+mr-teddy-bear@users.noreply.github.com>
2022-07-28 12:44:42 +0300

    fix: tooltip and centr text (#14)

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -4,3 +4,4 @@ export { ReactComponent as BTCIcon } from './btc.svg';
 export { ReactComponent as MetaMaskSmallIcon } from './MetaMaskSmallIcon.svg';
 export { ReactComponent as MinterestSmallicon } from './MinterestSmallicon.svg';
 export { ReactComponent as CloseIcon } from './closeIcon.svg';
+export { ReactComponent as TooltipQuestion } from './TooltipQuestion.svg';
